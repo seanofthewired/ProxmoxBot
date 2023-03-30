e9e4a4ef02f2703c0f9eb823dc2fde07e30232cf
seanofthewired <129461017+seanofthewired@users.noreply.github.com>
2023-03-30 17:46:53 -0600

    Add files via upload
    Responses: This is the logic for how the bot should call the Proxmox class based on what the user typed in a text channel.
    
    DiscordBot: Constructor for the Discord bot. 
    
    Proxmox: The bulk of this class is for generating CSRFPreventionToken and making API calls to the Proxmox server. 
    
    ProxmoxDiscordBot: Main class

---
+++ Proxmox.py
@@ -0,0 +1,110 @@
+from proxmoxer import ProxmoxAPI
+import requests
+import json
+import math
+
+game_vms = {
+    "VM Name": VM ID,
+    "VM Name": VM ID,
+    "VM Name": VM ID,
+    }
+
+num_vms = {
+    "ID":"ID",
+    "ID":"ID",
+    "ID":"ID",
+    }
+
+def ProxmoxToken():
+    url = 'https://IP Address of Proxmox goes here:8006/api2/json/access/ticket'
+    data = {'username': 'API@pve', 'password': 'Password of Proxmox Account'}
+    print (data)
+    response = requests.post(url, json=data, allow_redirects=True, verify=False)
+    jsonResponse = response.json()
+    return jsonResponse
+
+def get_memory_ussage(vm_id):
+    jsonResponse = ProxmoxToken()
+    vm_id_str = str(vm_id)
+    url = 'https://IP Address of Proxmox goes here:8006/api2/json/nodes/Proxmox Node Name Goes Here/qemu/' + vm_id_str + '/status/current'
+   
+    CSRFPreventionToken = jsonResponse["data"]["CSRFPreventionToken"]
+    ticket = jsonResponse["data"]["ticket"]
+    cookies = {'PVEAuthCookie': ticket}
+    headers = {'CSRFPreventionToken': CSRFPreventionToken}
+
+    data = requests.get(url, cookies=cookies, headers=headers, allow_redirects=True, verify=False)
+    data = data.json()
+
+    return data["data"]["mem"], data["data"]["maxmem"]
+
+
+def get_status_all():
+    messages = []
+    for key, value in game_vms.items():
+        messages.append(key +"\t" + str(value) +"\t" + StatusVirtualMachine(value) +"\t" + str(bytes_to_gb(get_memory_ussage(value)[0])) + "GB / " + str(bytes_to_gb(get_memory_ussage(value)[1])) + "GB")
+    return "\n".join(messages)
+
+def bytes_to_gb(bytes):
+    gb = bytes / 1073741824
+    return round(gb, 2)
+
+def StatusVirtualMachine(VMid):
+    print(type(VMid))
+    jsonResponse = ProxmoxToken()
+    VMid = str(VMid)
+
+    num = str(num_vms[VMid])
+    
+    url = 'https://IP Address of Proxmox goes here:8006/api2/json/nodes/Proxmox Node Name Goes Here/qemu/' + num + '/status/current'
+    CSRFPreventionToken = jsonResponse["data"]["CSRFPreventionToken"]
+    ticket = jsonResponse["data"]["ticket"]
+    cookies = {'PVEAuthCookie': ticket}
+    headers = {'CSRFPreventionToken': CSRFPreventionToken}
+
+    x = requests.get(url, cookies=cookies, headers=headers, allow_redirects=True, verify=False)
+    x = x.json()
+    status = x["data"]["qmpstatus"]
+    return status
+
+def StartVirtualMachine(VMid):
+    jsonResponse = ProxmoxToken()
+    VMid = str(VMid)
+    num = num_vms[VMid]
+    url = 'https://IP Address of Proxmox goes here:8006/api2/json/nodes/Proxmox Node Name Goes Here/qemu/' + num + '/status/start'
+   
+    CSRFPreventionToken = jsonResponse["data"]["CSRFPreventionToken"]
+    ticket = jsonResponse["data"]["ticket"]
+    cookies = {'PVEAuthCookie': ticket}
+    headers = {'CSRFPreventionToken': CSRFPreventionToken}
+
+    x = requests.post(url, cookies=cookies, headers=headers, allow_redirects=True, verify=False)
+
+    print(x.text)
+
+def StopVirtualMachine(VMid):
+    jsonResponse = ProxmoxToken()
+    VMid = str(VMid)
+    num = num_vms[VMid]
+    url = 'https://IP Address of Proxmox goes here:8006/api2/json/nodes/Proxmox Node Name Goes Here/qemu/' + num + '/status/stop'
+   
+    CSRFPreventionToken = jsonResponse["data"]["CSRFPreventionToken"]
+    ticket = jsonResponse["data"]["ticket"]
+    cookies = {'PVEAuthCookie': ticket}
+    headers = {'CSRFPreventionToken': CSRFPreventionToken}
+
+    x = requests.post(url, cookies=cookies, headers=headers, allow_redirects=True, verify=False)
+
+    print(x.text)
+
+def InfoVirtualMachine(VMid):
+    jsonResponse = ProxmoxToken()
+    VMid = str(VMid)
+    url = 'https://IP Address of Proxmox goes here:8006/api2/json/nodes/Proxmox Node Name Goes Here/qemu/' + VMid + '/status/start'
+   
+    CSRFPreventionToken = jsonResponse["data"]["CSRFPreventionToken"]
+    ticket = jsonResponse["data"]["ticket"]
+    cookies = {'PVEAuthCookie': ticket}
+    headers = {'CSRFPreventionToken': CSRFPreventionToken}
+
+    x = requests.post(url, cookies=cookies, headers=headers, allow_redirects=True, verify=False)
---
+++ ProxmoxDiscordBot.py
@@ -0,0 +1,5 @@
+import Proxmox
+import DiscordBot
+
+if __name__ == '__main__':
+    DiscordBot.run_discord_bot()
